44eb9b779af45816e288c0d2d643d94ddcbf9b13
Charlessnow <ekekwecharles50@gmail.com>
2023-10-02 13:08:54 +0100

    finished project

--- celeb_project/celeb_app/urls.py
+++ celeb_project/celeb_app/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.show_celebrities, name='show_celebrities'),
     path('vote/', views.vote_celebrity, name='vote_celebrity'),
-    path('token/', views.enter_token, name='enter_token'),
-    path('thanks/', views.thank_you, name='thank_you'),
+    path('enter_token/', views.enter_token, name='enter_token'),
 ]
--- celeb_project/celeb_app/views.py
+++ celeb_project/celeb_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Celebrity
+from django.contrib import messages
 
 def show_celebrities(request):
     celebrities = Celebrity.objects.all()
@@ -11,8 +12,30 @@ def vote_celebrity(request):
     # Update the votes for the selected celebrity
     return redirect('show_celebrities')
 
-def enter_token(request):
-    return render(request, 'enter_token.html')
 
-def thank_you(request):
-    return render(request, 'thank_you.html')
+def enter_token(request):
+    celebrity_id = request.GET.get('celebrity_id')
+    token = request.GET.get('token')
+
+    # Assuming celebrity_id is an integer and corresponds to a valid Celebrity
+    celebrity = Celebrity.objects.get(id=celebrity_id)
+
+    # celebrity_id = int(celebrity_id)
+
+    if celebrity_id == '2':
+        if token and token.startswith('E') and token.endswith('C') and token[1:-1].isdigit():
+            # Token is valid, increase votes for the celebrity
+            celebrity.votes += 1
+            celebrity.save()
+            # return render(request, 'thank_you.html')
+            messages.success(request, "Thanks for your vote, You are Indeed a real fan❤❤❤ \nRefer to your friends for more votes")
+        else:
+            messages.error(request, 'Incorrect Token')
+
+    else:
+        messages.error(request, 'This Poll link is only for Emilia Clarke, find alternate links to vote for others')
+    # Token is invalid, redirect to an error page or handle it accordingly
+    # return render(request, 'invalid_token.html')
+
+    return redirect('show_celebrities')
+
